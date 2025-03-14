3e181a8076449c4cb4d348bd04da379146a7b805
joel pamo <jpamo@unsa.edu.pe>
2025-03-13 21:38:18 -0500

    segunda version

---
+++ data_transform.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Ruta del archivo CSV intermedio
+archivo_csv = 'clientes.csv'
+
+try:
+    # Leer el archivo CSV
+    data = pd.read_csv(archivo_csv)
+    
+    # Ordenar los datos por nombre
+    data_ordenada = data.sort_values(by='Nombre1')
+    # Regla 2: transformar datos null por 0.
+    # Regla 3: Eliminar tercer nombre  de cada registro
+    
+    # Exportar a Excel
+    archivo_excel = 'clientes_ordenados.xlsx'
+    data_ordenada.to_excel(archivo_excel, index=False)
+    
+    print(f"Datos exportados exitosamente a {archivo_excel}")
+except Exception as e:
+    print(f"Error al transformar los datos: {e}")
